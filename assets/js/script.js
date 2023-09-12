77e1b7f543ad889da99534e0b005217f32eb12ba
Nelson Ramirez <xhyskoax@gmail.com>
2023-09-12 12:00:59 -0300

    Se termina formulario y tarjeta que muestra detalles del pokémon buscado

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,19 +24,52 @@ $(()=> {
             return response.json();
         })
         .then(function(data){
-            console.log(data);
+            //EN DATA NOS LLEGA LA INFORMACIÓN DEL POKÉMON CONSULTADO
+            //console.log(data);
+            let habilidades = data.abilities.map(habilidad => {
+                return habilidad.ability.name
+            })
+            
+            //console.log(habilidades);
+
+            let pokemon = {
+                nombre: data.name,
+                imagen: data.sprites.other.dream_world.front_default,
+                habilidades: habilidades //array que contiene nombre de habilidades
+            }
+            console.log(pokemon);
+            cargarDatosPokemonCard(pokemon);
         })
         .catch(function(error){
             //CAPTURAMOS ERROR Y LO MOSTRAMOS EN UNA VENTANA EMERGENTE
             console.log(error);
             alert(error);
         })
-        
-
-
-
     })
 
+
+    function cargarDatosPokemonCard(pokemon){
+        //se reemplaza imagen y alt que tiene el card por la imagen pokemon buscado y nombre
+        $("#cardPokemon > img").attr("src", pokemon.imagen);
+        $("#cardPokemon > img").attr("alt", pokemon.nombre);
+        $("#cardPokemon .card-title").text(pokemon.nombre.toUpperCase());
+        
+        //SE CREA UN ACUMULADOR DE ELEMENTOS LI
+        let liHabilidades = "";
+
+        //RE RECORRE ARRAY DE HABILIDADES PARA CREAR TANTOS LI COMO HABILIDADES TENGA
+        //EL POKÉMON
+        pokemon.habilidades.forEach(function(habilidad){
+            liHabilidades += `
+                <li>${habilidad}</li>
+            `
+        })
+
+        //LE AGREGAMOS LOS ELEMENTOS DE LISTA A LA UL (LISTA)
+        $("#listaHabilidades").html(liHabilidades);
+
+
+    }
 
 
 })
